8183d2e69cf4eb8f0127e89e266136ca290d78d9
TomimotoV <137180358+TomimotoV@users.noreply.github.com>
2024-07-02 14:14:30 +0000

    Desafio: Novo endpoint /oi

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+// DESAFIO: Criar novo endpoint /oi com "Olá, mundo!"
+app.get('/oi', function (req, res) {
+    res.send('Olá, mundo!')
+  })
+
 app.listen(3000)
